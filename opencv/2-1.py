49f252a4f9e7c29c070d790f74278146b43e0bc6
zh <zh1047049332@gmail.com>
2022-11-26 23:07:06 +0900

    something changed

--- opencv/2-1.py
+++ opencv/2-1.py
@@ -4,16 +4,16 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv.imread('2.jpeg',1)
+img = cv.imread('Photos/2.jpeg',1)
 if img is None:
     print('the file path is wrong')
 
-'''    
+  
 cv.imshow('image',img)
 cv.waitKey(0)
 cv.destroyWindow('image')
-'''
+'''  
 plt.imshow(img,cmap='gray',interpolation='bicubic')
 plt.xticks([]),plt.yticks([])
 plt.show()
-
+'''
